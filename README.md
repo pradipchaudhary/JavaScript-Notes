138558c2fc41891f9ca66dbac3f23537da3eaee2
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-05-25 12:20:51 +0545

    JavaScript String Methods ...

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # javascript-notes
+
 Javascript note for remember to learn.
+
+1. String
+2. Number
--- String/methods.js
+++ String/methods.js
@@ -1,5 +1,31 @@
-// JavaScript String Method
-// 1. length
+// 1. charAt(index): Returns the character at the specified index in a string.
 
-const name = "Pradip chaudhary";
-console.log(name.length);
+// 2. charCodeAt(index): Returns the Unicode value of the character at the specified index in a string.
+
+// 3. concat(str1, str2, …, strN): Joins two or more strings and returns a new string.
+
+// 4. endsWith(searchString, length): Checks if a string ends with the specified substring and returns a boolean value.
+
+// 5. includes(searchString, position): Checks if a string contains the specified substring and returns a boolean value.
+
+// 6. indexOf(searchValue, fromIndex): Returns the index of the first occurrence of a specified value in a string.
+
+// 7. lastIndexOf(searchValue, fromIndex): Returns the index of the last occurrence of a specified value in a string.
+
+// 8. length: Returns the length of a string.
+
+// 9. replace(searchValue, replaceValue): Replaces a specified value or a regular expression with a replacement value.
+
+// 10. slice(startIndex, endIndex): Extracts a portion of a string and returns a new string.
+
+// 11. split(separator, limit): Splits a string into an array of substrings based on a specified separator.
+
+// 12. startsWith(searchString, position): Checks if a string starts with the specified substring and returns a boolean value.
+
+// 13. substring(startIndex, endIndex): Extracts the characters between two specified indices and returns a new string.
+
+// 14. toLowerCase(): Converts a string to lowercase.
+
+// 15. toUpperCase(): Converts a string to uppercase.
+
+// 16. trim(): Removes whitespace from both ends of a string.
