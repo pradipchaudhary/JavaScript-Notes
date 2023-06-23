ba5404cdd485d7dcdfce02d68e445d644f1c1dfe
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-06-23 11:56:28 +0545

    add scs in javascript notes

--- String/methods.js
+++ String/methods.js
@@ -29,3 +29,32 @@
 // 15. toUpperCase(): Converts a string to uppercase.
 
 // 16. trim(): Removes whitespace from both ends of a string.
+
+// Certainly! Here is a comprehensive list of JavaScript string methods:
+
+// 1. `charAt(index)`: Returns the character at the specified index in a string.
+// 2. `charCodeAt(index)`: Returns the Unicode value of the character at the specified index in a string.
+// 3. `concat(str1, str2, ..., strN)`: Joins two or more strings and returns a new string.
+// 4. `endsWith(searchString, length)`: Checks if a string ends with the specified substring and returns a boolean value.
+// 5. `includes(searchString, position)`: Checks if a string contains the specified substring and returns a boolean value.
+// 6. `indexOf(searchValue, fromIndex)`: Returns the index of the first occurrence of a specified value in a string.
+// 7. `lastIndexOf(searchValue, fromIndex)`: Returns the index of the last occurrence of a specified value in a string.
+// 8. `length`: Returns the length of a string.
+// 9. `localeCompare(compareString)`: Compares two strings in the current locale and returns a number indicating their relative order.
+// 10. `match(regexp)`: Searches a string for a match against a regular expression and returns an array of matches.
+// 11. `replace(searchValue, replaceValue)`: Replaces a specified value or a regular expression with a replacement value.
+// 12. `search(regexp)`: Searches a string for a specified value or a regular expression and returns the index of the match.
+// 13. `slice(startIndex, endIndex)`: Extracts a portion of a string and returns a new string.
+// 14. `split(separator, limit)`: Splits a string into an array of substrings based on a specified separator.
+// 15. `startsWith(searchString, position)`: Checks if a string starts with the specified substring and returns a boolean value.
+// 16. `substr(startIndex, length)`: Extracts a specified number of characters from a string, starting from a specified index.
+// 17. `substring(startIndex, endIndex)`: Extracts the characters between two specified indices and returns a new string.
+// 18. `toLocaleLowerCase()`: Converts a string to lowercase based on the current locale.
+// 19. `toLocaleUpperCase()`: Converts a string to uppercase based on the current locale.
+// 20. `toLowerCase()`: Converts a string to lowercase.
+// 21. `toUpperCase()`: Converts a string to uppercase.
+// 22. `toString()`: Returns the string representation of an object.
+// 23. `trim()`: Removes whitespace from both ends of a string.
+// 24. `valueOf()`: Returns the primitive value of a string object.
+
+// These are the commonly used string methods in JavaScript. Each method serves a specific purpose and allows you to perform various operations on strings.
