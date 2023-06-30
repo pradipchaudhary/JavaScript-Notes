72c8e76697185f728bef1bee6f9159fe287c47b1
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-07-01 04:02:19 +0545

    slice and length string method

---
+++ String Methods/02_slice.js
@@ -0,0 +1,6 @@
+const news =
+    "Sri Lanka and the Netherlands will face each other in a high-octane clash of the Super Six stage of the World Cup Qualifier on June 30. Both sides secured their place in the Super Six and are still in contention for booking a slot in the World Cup.";
+
+const newsTitle = news.slice(200);
+console.log(news);
+console.log("Title: " + newsTitle);
