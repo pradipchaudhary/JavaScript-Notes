5c3b861dfa1b2605c67d75f222b2717c7fbfdee5
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-07-01 20:17:17 +0545

    String length check without using length method.

--- String Methods/01_length.js
+++ String Methods/01_length.js
@@ -4,3 +4,16 @@ const news =
 const newsLength = news.length;
 console.log(news);
 console.log(newsLength);
+
+// Without inbuilt methods
+function getStringLength(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        count++;
+    }
+    return count;
+}
+
+let myString = "Hello, world!";
+let stringLength = getStringLength(myString);
+console.log(stringLength); // Output: 13
