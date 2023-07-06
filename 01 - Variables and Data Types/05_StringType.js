0d4a333d75ae898b13aa117cd4810495934bc9c3
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-07-06 20:17:49 +0545

    converting to string in JavaScript Note

--- 01 - Variables and Data Types/05_StringType.js
+++ 01 - Variables and Data Types/05_StringType.js
@@ -4,5 +4,36 @@
 
 var firstName = "Pradip";
 var lastName = "Chaudhary";
-console.log(firstName);
-console.log(typeof firstName);
+// console.log(firstName);
+// console.log(typeof firstName);
+
+/*
+ * Character Literals
+ * The String data type includes several character literals to represent nonprintable or otherwise useful
+ *  characters, as listed in the following table:
+ *
+ */
+
+console.log("Hello, \n Pradip"); // \n New line
+console.log("this is a tab \t character"); // Tab
+
+// Properties
+var fullName = "Pradip Chaudhary";
+console.log(fullName);
+console.log(fullName.length);
+
+// Converting to a String
+// There are two ways to convert a value into a string.
+// 1. toString()
+console.log("Converting to String: ");
+var age = 30;
+var ageAsString = age.toString();
+console.log(age, typeof age);
+console.log(ageAsString, typeof ageAsString);
+
+var found = true;
+var foundAsString = found.toString();
+console.log(found, typeof found);
+console.log(foundAsString, typeof foundAsString);
+
+// If a value is null or undefined, this method is not available.
