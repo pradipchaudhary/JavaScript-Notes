c04d7b433a695665db87576acb6258401ac7bb8c
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-07-07 20:42:29 +0545

    Operators in JavaScript

---
+++ 01 - Variables and Data Types/Operators/01_UnaryOperators.js
@@ -0,0 +1,7 @@
+// Unary Operators
+// Operators that work on only one value are called unary operators. They are the simplest operators in ECMAScript.
+
+// 1. Increment/Decrement
+var age = 20;
+++age;
+console.log(age);
