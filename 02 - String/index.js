25a1463c33b8ecc778c2990e240c04a7ca38aa50
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-08-24 18:58:28 +0545

    add readme file .

--- 02 - String/index.js
+++ 02 - String/index.js
@@ -1,1 +1,2 @@
-console.log("String ");
+// What is the string?
+// ==> In computer programming, a string is a sequence of characters used to represent text. It is typically used to store and manipulate text data.
