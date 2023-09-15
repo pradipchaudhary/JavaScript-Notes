84525f72a1dd51018a3c63bce4558630880db4ab
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-09-15 15:36:03 +0545

    add array method.

---
+++ 03 - Arrary/Methods/01_push.js
@@ -0,0 +1,12 @@
+// Array.prototype.push()
+// ==> The push() method of Array instances adds the specified elements to the end of an array and returns the new length of the array.
+// push()
+// push(element1)
+// push(element1, element2)
+// push(element1, element2, /* …, */ elementN)
+
+const person = ["John", "Younz"];
+console.log(person);
+
+person.push("Ram");
+console.log(first);
---
+++ 03 - Arrary/Methods/02_pop.js
@@ -0,0 +1,6 @@
+const num = [2, 3, 4, 5, 6];
+console.log(num);
+console.log(num.length);
+num.pop();
+console.log(num);
+console.log(num.length);
