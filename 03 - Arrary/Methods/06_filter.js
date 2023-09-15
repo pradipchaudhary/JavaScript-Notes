2ac2e8acda3ef82476a752d37ea45addbd53d500
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-09-15 16:07:47 +0545

    add filter method in array methods.

---
+++ 03 - Arrary/Methods/06_filter.js
@@ -0,0 +1,18 @@
+// This method creates a new array with only elements that passes the condition inside the provided function.
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(numbers);
+// numbers.filter(function (number) {
+//     //     console.log(number);
+//     if (number < 6) {
+//         console.log(number);
+//     }
+// });
+
+const fruits = ["Apple", "Orange", "Banana", "Papaya"];
+console.log(fruits);
+fruits.filter(function (fruit) {
+    if ((fruit == "Apple") | (fruit == "Orange")) {
+        console.log(fruit);
+        //   console.log("hello");
+    }
+});
