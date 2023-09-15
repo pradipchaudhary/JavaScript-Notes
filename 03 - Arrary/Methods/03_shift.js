2dce0859b90fa8d94f8395f74e699e19c9df37aa
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-09-15 15:41:46 +0545

    add shift and unshift method in array.

---
+++ 03 - Arrary/Methods/03_shift.js
@@ -0,0 +1,6 @@
+// This method removes the first element from an array and returns that element.
+
+const num = Array(1, 2, 3, 4, 5, 6, 7, 8);
+console.log(num);
+num.shift();
+console.log(num);
---
+++ 03 - Arrary/Methods/04_unshift.js
@@ -0,0 +1,6 @@
+// This method adds one or more elements to the beginning of an array and returns the new length of the array.
+
+const num = [1, 2, 3];
+console.log(num);
+num.unshift(4);
+console.log(num);
