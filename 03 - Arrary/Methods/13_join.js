26b478c81c1424c6b930c339bdde54e790bc44dc
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-09-17 03:04:13 +0545

    add join method in array methods.

---
+++ 03 - Arrary/Methods/13_join.js
@@ -0,0 +1,14 @@
+// This method returns a new string by concatenating all of the array’s elements separated by the specified separator.
+
+const names = ["hari", "Sara", "Sita"];
+console.log(names);
+
+const joinByDashes = names.join("-");
+console.log(joinByDashes);
+console.log(typeof joinByDashes);
+
+const numbers = Array(1, 2, 3, 4, 5);
+
+const joinAll = numbers.join("");
+console.log(joinAll);
+console.log(typeof joinAll);
